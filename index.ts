62c5721577b845a564536d28d34f6d6e810fb63e
Matas Ubarevicius <matas@bitbybit.dev>
2023-01-30 16:13:48 +0200

    occt library of bitbybit.dev

---
+++ index.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of bitbybit-core
+ */
+
+export * from './lib/api/inputs';
+export * from './lib/api/vector-helper.service'
+export * from './lib/api/shapes-helper.service'
+export * from './lib/occ-service';
+export * from './lib/occ-helper';
+export * from './bitbybit-dev-occt/bitbybit-dev-occt';
