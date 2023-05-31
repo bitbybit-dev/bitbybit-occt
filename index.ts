6d19171a5eb204ac70f3df34881172a1c410d56a
Matas Ubarevicius <matas@bitbybit.dev>
2023-05-31 17:56:14 +0300

    v0.11.6 and export through highest index.ts

--- index.ts
+++ index.ts
@@ -2,10 +2,5 @@
  * Public API Surface of bitbybit-core
  */
 
-export * from './lib/api/inputs';
-export * from './lib/api/outputs';
-export * from './lib/api/vector-helper.service'
-export * from './lib/api/shapes-helper.service'
-export * from './lib/occ-service';
-export * from './lib/occ-helper';
+export * from './lib';
 export * from './bitbybit-dev-occt/bitbybit-dev-occt';
